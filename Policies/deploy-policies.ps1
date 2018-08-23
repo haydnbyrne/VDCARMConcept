cf71704df480260a5bb1edcbda4c8ed351d83ff8
haydnbyrne <habyrne@microsoft.com>
2018-08-23 15:33:44 +1000

    about to make big changes

--- Policies/deploy-policies.ps1
+++ Policies/deploy-policies.ps1
@@ -1,5 +1,4 @@
-#Save-Module -Name AzureRM.ManagementGroups -Path C:\Repos\VDCARM\Policies\modules
-#Install-Module -Name AzureRM.ManagementGroups -Scope CurrentUser
 Import-Module -Name AzureRM.Resources
-$Definition = New-AzureRmPolicyDefinition -DisplayName "IAG Allowed Locations" -Name "IAGAllowedLocations" -Description "Only allow deployment of resources in specified regions" -Policy .\Policies\allowedlocations.json
+$Definition = New-AzureRmPolicyDefinition -DisplayName "IAG Allowed Locations" -Name "IAGAllowedLocations" -Description "Only allow deployment of resources in specified regions" -Policy "$($PSScriptRoot)Policies\allowedlocations.json"
 #New-AzureRmPolicyAssignment -PolicyDefinition $Definition -Scope
+Get-AzureRmPolicyAssignment
